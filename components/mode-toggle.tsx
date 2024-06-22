7488144bf382d5010c5b76a1822644e308b3c395
aimamit <amitkumar@MACN-AMKU.local>
2024-06-22 18:10:05 +0530

    SEO related changes

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -10,8 +10,14 @@ export function ModeToggle() {
 
   return (
     <Toggle onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      <Sun className="transition-all scale-100 rotate-0 dark:scale-0 dark:-rotate-90 h-[1.2rem] w-[1.2rem]" />
-      <Moon className="absolute transition-all scale-0 rotate-90 dark:scale-100 dark:rotate-0 h-[1.2rem] w-[1.2rem]" />
+      <Sun
+        className="transition-all scale-100 rotate-0 dark:scale-0 dark:-rotate-90 h-[1.2rem] w-[1.2rem]"
+        aria-label="light-theme-button"
+      />
+      <Moon
+        className="absolute transition-all scale-0 rotate-90 dark:scale-100 dark:rotate-0 h-[1.2rem] w-[1.2rem]"
+        aria-label="dark-theme-button"
+      />
     </Toggle>
   );
 }
